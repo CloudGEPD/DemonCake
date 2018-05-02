ead4dd1936e2e0a411f7901d31da62b80e14661c
CloudGEPD <38668514+CloudGEPD@users.noreply.github.com>
2018-05-02 13:14:09 -0500

    Designing more rooms
    Finishing second level

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,25 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-[AddComponentMenu("Controls/Player Movement")]
+//[AddComponentMenu("Controls/Player Movement")]
 
 public class Movement : MonoBehaviour
 {
-    public float speed0 = 0;
-    public float speed1 = 0;
+    public float speed0 = 1.0f;
 
     private void Update()
     {
         if (Input.GetButton("Dash"))
         {
-            transform.Translate(Vector3.up * Time.deltaTime * Input.GetAxis("Vertical") * speed1);
-            transform.Translate(Vector3.right * Time.deltaTime * Input.GetAxis("Horizontal") * speed0);
+            transform.Translate(transform.forward * Input.GetAxis("Vertical"));
+            transform.Translate(transform.right *Input.GetAxis("Horizontal"));
         }
-        else
-        {
-            transform.Translate(Vector3.up * Time.deltaTime * Input.GetAxis("Vertical") * speed0);
-            transform.Translate(Vector3.right * Time.deltaTime * Input.GetAxis("Horizontal") * speed0);
-        }
+        float h = speed0 * (Input.GetAxis("A, D"));
+     
     }
     
 }
