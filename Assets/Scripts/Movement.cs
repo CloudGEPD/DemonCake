2a953684c88ea80598c49e0b7587f160fad261bc
CloudGEPD <38668514+CloudGEPD@users.noreply.github.com>
2018-05-15 15:10:29 -0500

    Lets see if this works
    Should be updated

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,21 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-[AddComponentMenu("Controls/Player Movement")]
+//[AddComponentMenu("Controls/Player Movement")]
 
 public class Movement : MonoBehaviour
 {
-    public float speed0 = 1.0f;
+    Transform mc;
+    public float speed0;
 
     private void Update()
     {
-        if (Input.GetButton("Dash"))
+        
         {
-            transform.Translate(transform.forward * Input.GetAxis("Vertical"));
-            transform.Translate(transform.right *Input.GetAxis("Horizontal"));
+            mc.Translate(transform.forward * Input.GetAxis("Vertical"));
+            mc.Translate(transform.right *Input.GetAxis("Horizontal"));
         }
-        float h = speed0 * (Input.GetAxis("A, D"));
+        //float h = speed0 * (Input.GetAxis("A, D"));
      
+    }
+    void HorizontalSpeed()
+    {
+       // float h 
     }
     
 }
